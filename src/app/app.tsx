9fa7344777ac968f37865bf4a3348f9a3d5a6ab3
Vitaliy <egoshinvit@gmail.com>
2025-09-20 22:06:18 +0700

    refactor: change app styles and add widgets

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,24 @@
 import { FC } from 'react';
 
+import clsx from 'clsx';
 import { Outlet } from 'react-router';
 
+import s from './app.module.scss';
+
 import { Header } from '@/widgets/header';
+import { Sidebar } from '@/widgets/sidebar';
 
 export const App: FC = () => {
+  const IS_PLAYER_OPEN = true;
+
   return (
-    <>
+    <div className={clsx(s.grid, IS_PLAYER_OPEN && s.playerOpen)}>
       <Header />
-      <Outlet />
-    </>
+      <Sidebar />
+      <main className={s.main}>
+        <Outlet />
+      </main>
+      {/* {IS_PLAYER_OPEN && <Player />} */}
+    </div>
   );
 };
