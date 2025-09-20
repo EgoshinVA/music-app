0348a71640edc9a5dceb65271e8c37b9aacd1829
Vitaliy <egoshinvit@gmail.com>
2025-09-20 22:06:06 +0700

    refactor: change header styles

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,25 +1,19 @@
 import { FC } from 'react';
 
-import { NavLink } from 'react-router';
-
 import s from './header.module.scss';
 
-import { navItems } from '@/widgets/header/lib/nav-items';
+// const IS_AUTH = true; // temporary data
 
 export const Header: FC = () => {
   return (
-    <header className={s.container}>
-      <nav>
-        <ul className={s.list}>
-          {navItems.map(item => (
-            <li key={item.to}>
-              <NavLink to={item.to} className={({ isActive }) => `link ${isActive ? s.activeLink : ''}`}>
-                {item.label}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <header className={s.header}>
+      <div>music-app</div>
+
+      {/* {IS_AUTH ? ( */}
+      {/*  <ProfileDropdownMenu avatar="//unsplash.it/100/100" name="Martin Fowler" id="1" /> */}
+      {/* ) : ( */}
+      {/*  <LoginButtonAndModal /> */}
+      {/* )} */}
     </header>
   );
 };
