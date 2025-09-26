f99d16553ef63edb3d0e3c12630ed122a02f3178
Vitaliy <egoshinvit@gmail.com>
2025-09-26 09:31:41 +0700

    refactor: refactor base api

--- src/shared/api/base-api.ts
+++ src/shared/api/base-api.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  tagTypes: ['Playlist'],
+  tagTypes: ['Playlist', 'Tag'],
+  refetchOnFocus: true,
+  refetchOnReconnect: true,
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_BASE_URL,
     prepareHeaders: headers => {
